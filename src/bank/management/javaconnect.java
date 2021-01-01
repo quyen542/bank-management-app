ce8e8e031c57426a9fe545e7fc9f937c1c39b146
phucide <69331153+phucide@users.noreply.github.com>
2021-01-01 15:29:37 +0700

    Update javaconnect.java

--- src/bank/management/javaconnect.java
+++ src/bank/management/javaconnect.java
@@ -1,1 +1,29 @@
-abc
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank.management;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public interface javaconnect {
+    Connection conn = null;
+    
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:Bank-m.db");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;
+    }
+}
